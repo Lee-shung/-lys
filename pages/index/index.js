3bcb65b63fef176f0082680b0329be2e6d18be93
Lee-shung <18684824230@163.com>
2020-03-31 20:22:13 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,102 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    books: [
+      {
+        img:'/images/jinyong2.jpg',
+        name:'金庸作品全集 (新修版) (全36册) (金庸作品集)',
+        author:'金庸',
+        cate:'Kindle电子书',
+        price:499.00,
+        business:'自营',
+        isDownload:true
+      },
+      {
+        img:'/images/cun2.jpg',
+        name:'村上春树长篇代表作品集 (套装共10册)',
+        author:'村上春树(Haruki Muarakami)',
+        cate:'Kindle电子书',
+        price:99.99,
+        business:'自营',
+        isDownload:true
+      },
+      {
+        img:'/images/ka.jpg',
+        name:'卡尔维诺精选作品集 (套装22册) (王小波挚爱，与卡夫卡、马尔克斯、普鲁斯特齐名的世界一流大师)',
+        author:'伊塔洛•卡尔维诺',
+        cate:'Kindle电子书',
+        price:199.99,
+        business:'自营',
+        isDownload:true
+      },
+      {
+        img:'/images/ying.jpg',
+        name:'应物兄:全2册 (潜心创作13年，荣登2018《收获》文学排行榜榜首;向《红楼梦》致敬的重要收获)',
+        author:'李洱',
+        cate:'Kindle电子书',
+        price:19.99,
+        business:'自营',
+        isDownload:false
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
